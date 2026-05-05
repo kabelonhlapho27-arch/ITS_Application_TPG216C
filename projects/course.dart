ce484dc95a78c56584be5b8ee9e2ab6d24b3fd7b
kabelonhlapho27-arch <kabelonhlapho27@icloud.com>
2026-05-05 17:09:47 +0000

    Implement Course class with properties, constructor, getters, and setters

--- projects/course.dart
+++ projects/course.dart
@@ -1,2 +1,50 @@
-void course()
-{}
+class Course
+{
+  String courseCode;
+  String courseName;
+  int durationInWeeks = 0;
+  String level;
+
+  //Constructor
+  Course(String code, String name, int duration, String courseLevel)
+  {
+    courseCode = code;
+    courseName = name;
+    durationInWeeks = duration;
+    level = courseLevel;
+  }
+
+  //Getters methods
+  String get courseCode{
+    return courseCode;
+  }
+
+  String get courseName{
+    return courseName;
+  }
+
+  int get durationInWeeks{
+    return durationInWeeks;
+  }
+
+  String get level{
+    return level;
+  }
+
+ //Setters
+  set courseCode(String code){
+    courseCode = code;
+  }
+
+  set courseName(String name){
+    courseName = name;
+  }
+
+  set durationInWeeks(int duration){
+    durationInWeeks = duration;
+  }
+
+  set level(String courseLevel){
+    level = courseLevel;
+  }
+}
